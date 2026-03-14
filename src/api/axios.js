44e6c510fd0630efa6f875ed7489822ab12e2d37
Rahul3658 <rahuldc3658@gmail.com>
2026-03-14 14:43:20 +0530

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,21 +1,38 @@
 import axios from 'axios';
 
-// Determine which API URL to use based on the flag
-const useProduction = process.env.REACT_APP_USE_PRODUCTION === 'true';
-const apiUrl = useProduction 
-  ? process.env.REACT_APP_PROD_API_URL 
-  : process.env.REACT_APP_DEV_API_URL;
-
 const API = axios.create({
-  baseURL: apiUrl,
+  baseURL: "/api",
 });
 
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
+
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
+
   return config;
 });
 
 export default API;
+// import axios from 'axios';
+
+// // Determine which API URL to use based on the flag
+// const useProduction = process.env.REACT_APP_USE_PRODUCTION === 'true';
+// const apiUrl = useProduction 
+//   ? process.env.REACT_APP_PROD_API_URL 
+//   : process.env.REACT_APP_DEV_API_URL;
+
+// const API = axios.create({
+//   baseURL: apiUrl,
+// });
+
+// API.interceptors.request.use((config) => {
+//   const token = localStorage.getItem('token');
+//   if (token) {
+//     config.headers.Authorization = `Bearer ${token}`;
+//   }
+//   return config;
+// });
+
+// export default API;
